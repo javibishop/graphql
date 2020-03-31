c34fb67c0c9b3e85c2579051ffd14165e3feedb1
global <javibishop@gmail.com>
2020-03-31 18:48:25 -0300

    Cambios para apollo server

--- src/server.ts
+++ src/server.ts
@@ -3,55 +3,23 @@ import compression from 'compression';
 import cors from 'cors';
 import graphQLHTTP from 'express-graphql';
 import myschema from './schema/index'
+import {ApolloServer} from 'apollo-server-express';
+import { createServer } from 'http';
+
 const port = 3000;
 
 const app = express();
 app.use('*', cors());
 app.use(compression()); /*ver para que es */
 
-//definicion de los queries que hacepta mi api gql
-const  typeDefs = `
-    // type Query {
-    //     hola: String!
-    //     holaConNombre(nombre: String!): String!
-    //     holaAlCurso: String!
-    // }
-`;
-
-//definicion resolver que le dan a cada queries que hacepta mi api gql
-// const resolvers : IResolvers = {
-// // aca se pone con que se trabaja.
-//     Query: {
-//         hola(): string{
-//             return 'Hola Mundo';
-//         },
-//         holaConNombre(__: void, {
-//             nombre
-//         }): string{
-//             return `Hola Mundo ${nombre}`;
-//         },
-//         holaAlCurso(): string{
-//             return 'Hola Mundo cursograpql';
-//         }
-//     }
-// };
-
-//definicion de schema gql
-// const myschema: GraphQLSchema = makeExecutableSchema ({
-//     typeDefs,
-//     resolvers
-// });
-
-//en la raiz/graphql va a estar la api de gql
-app.use('/', graphQLHTTP({
+const server = new ApolloServer({
     schema: myschema,
-    graphiql: true
-}));
+    introspection:true
+});
 
-app.listen(port, (err) => {
+server.applyMiddleware({app});
+const httpServer = createServer(app);
 
-    if (err) throw new Error(err);
-
+httpServer.listen({port: port},() => {
     console.log(`Servidor corriendo en puerto ${ port }`);
-
 });
